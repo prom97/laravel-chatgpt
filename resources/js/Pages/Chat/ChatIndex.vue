04e72a9ff3c000ffd59752cd889c99fecf1c1f5c
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2023-04-13 11:39:04 -0300

    Display Chat Context History

--- resources/js/Pages/Chat/ChatIndex.vue
+++ resources/js/Pages/Chat/ChatIndex.vue
@@ -1,6 +1,7 @@
 <script setup>
 import ChatLayout from '@/Layouts/ChatLayout.vue';
-import { useForm } from '@inertiajs/vue3';
+import ChatContent from '@/Components/ChatContent.vue';
+import { useForm, Link } from '@inertiajs/vue3';
 
 const props = defineProps({
     messages: Array,
@@ -11,23 +12,42 @@ const form = useForm({
     promt: "",
 });
 
-const submit = () =>{
+const submit = () => {
     form.post("/chat");
 };
 </script>
 <template>
     <ChatLayout>
+        <!-- chat history list -->
         <template #aside>
+            <ul class="p-2">
+                <template v-for="message in messages" :key="message.id">
+                    <li
+                        class="px-4 py-2 my-2 flex justify-between font-semibold text-slate-400 bg-slate-900 hover:bg-slate-700 rounded-lg duration-200">
+                        <Link :href="`/chat/${message.id}`">{{ message.context[0].content }}</Link>
 
+                    </li>
+                </template>
+            </ul>
         </template>
         <div class="w-full flex text-white ">
-            <!--messages-history with chat-->
+            <!--messages response history with chat-->
+            <template v-if="chat">
+                <div class="w-full flex h-screen bg-slate-900">
+                    <div class="w-full overflow-auto">
+                        <template v-for="(content, index) in chat?.context" :key="index">
+                            <ChatContent :content="content" />
+                        </template>
+                    </div>
+                </div>
+            </template>
         </div>
         <template #form>
             <section class="px-6 top-0">
                 <div class="w-full">
                     <div class="relative flex-1 flex items-center">
-                        <input v-model="form.promt" @keyup.enter="submit" type="text" class="w-full bg-slate-700 text-white rounded-lg" placeholder="Ask laravel AI">
+                        <input v-model="form.promt" @keyup.enter="submit" type="text"
+                            class="w-full bg-slate-700 text-white rounded-lg" placeholder="Ask laravel AI">
                         <div class="absolute inset-y-0 right-0 flex items-center pl-3">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" class="w-6 h-6 -ml-8 text-slate-200">
@@ -39,4 +59,5 @@ const submit = () =>{
                 </div>
             </section>
         </template>
-    </ChatLayout></template>
+    </ChatLayout>
+</template>
